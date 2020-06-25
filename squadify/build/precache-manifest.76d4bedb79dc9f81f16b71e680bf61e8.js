c2f98d002291c376ef6405e079000553f1f4e091
geferson hess <12612525+Hessz@users.noreply.github.com>
2020-06-25 18:34:55 -0300

    Add files via upload

---
+++ squadify/build/precache-manifest.76d4bedb79dc9f81f16b71e680bf61e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d7ed6a33f11b28aefacee8e4d88def",
+    "url": "/squadify/index.html"
+  },
+  {
+    "revision": "b31ae91f4f70c3cded76",
+    "url": "/squadify/static/js/2.66993b6b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/squadify/static/js/2.66993b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4004de29a870a5c4fd",
+    "url": "/squadify/static/js/main.69053a68.chunk.js"
+  },
+  {
+    "revision": "ed142225e84a807fc368",
+    "url": "/squadify/static/js/runtime-main.594d4c62.js"
+  }
+]);
